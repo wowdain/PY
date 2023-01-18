2429264e0ca719afbef151789d0e6d42a5b212c2
laris <wowdainwow@gmail.com>
2023-01-18 21:06:11 +0900

    day03 prime number v0.7

--- day03.py
+++ day03.py
@@ -1,17 +1,19 @@
 # prime number v0.5
+# 두 개의 숫자 사이(입력된 두 수 포함)의 소수를 출력하는 프로그램
 
-number = int(input('input integer number : '))
-is_prime = True
+number_list = input('input 2 integer number : ').split()
+start = int(number_list[0]) #first number
+end = int(number_list[1]) #second number
 
-if number <= 1 :
-    is_prime = False
-else :
-    for k in range(2, number) :
-        if number % k == 0 :
-            is_prime = False
-            break
+for number in range(start, end+1):
+    is_prime = True
 
-if is_prime :
-    print(f'{number} is prime number.')
-else :
-    print(f'{number} is NOT prime number.')
+    if number <= 1 :
+        is_prime = False
+    else :
+        for k in range(2, number) :
+            if number % k == 0 :
+                is_prime = False
+                break
+        else :
+            print(number, end=' ')
