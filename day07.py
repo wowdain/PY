bb34167294561e75ea74a6a73e5f3990f5f84f27
laris <wowdainwow@gmail.com>
2023-01-26 18:59:03 +0900

    day07 function07

--- day07.py
+++ day07.py
@@ -1,19 +1,19 @@
-# Lambda function (after)
+def a():  # generator
+    yield 1
+    yield -9
+    yield 3
 
-import random
+def b():  # normal function
+    return 1  # stop
+    return -9
+    return 3
 
-def process(no_lists, f):
-    """
-    리스트와 함수를 받아 리스트의 원소들을 받은 함수에 모두 적용
-    :param no_lists: 리스트
-    :param f: 함수
-    :return: void
-    """
-    for no in no_lists:
-        print(f(no))
+print(type(a()), b())
+c = a()
+print(c)
 
+for i in c:
+    print(i)
 
-numbers = [random.randint(1, 10) for i in range(5)]
-print(numbers)
-process(numbers, lambda x: x * x)  # squares
-process(numbers, lambda x: x + x)  # doubles
+for i in c:
+    print(i)
